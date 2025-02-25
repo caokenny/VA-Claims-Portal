f8c0c56997f49b219aefb76044e08269f08e1233
Kenny <kennycao96@gmail.com>
2025-02-24 19:02:15 -0500

    Fixed ability to add Claimant__c record with Discharge_Type__c.

--- force-app/main/default/lwc/registrationForm/registrationForm.js
+++ force-app/main/default/lwc/registrationForm/registrationForm.js
@@ -9,18 +9,18 @@ export default class RegistrationForm extends LightningElement {
   phone = "";
   militaryBranch = "";
   veteranStatus = "";
-  // dischargeType = '';
+  dischargeType = "";
   errorMessage = "";
-  // showDischargeType = false;
+  showDischargeType = false;
 
   handleInputChange(event) {
     const field = event.target.dataset.id;
     this[field] = event.target.value;
     // console.log(`Field: ${field}, Value: ${this[field]}`);
 
-    // if (field === 'veteranStatus') {
-    //     this.showDischargeType = (this.veteranStatus !== 'Active Duty');
-    // }
+    if (field === "veteranStatus") {
+      this.showDischargeType = this.veteranStatus === "Discharged";
+    }
   }
 
   get branches() {
@@ -42,15 +42,18 @@ export default class RegistrationForm extends LightningElement {
     ];
   }
 
-  // get dischargeOptions() {
-  //     return [
-  //         {label: 'Honorable', value: 'Honorable'},
-  //         {label: 'General under Honorable Conditions', value: 'General under Honorable Conditions'},
-  //         {label: 'Other than Honorable', value: 'Other than Honorable'},
-  //         {label: 'Bad Conduct', value: 'Bad Conduct'},
-  //         {label: 'Dishonorable', value: 'Dishonorable'}
-  //     ];
-  // }
+  get dischargeOptions() {
+    return [
+      { label: "Honorable", value: "Honorable" },
+      {
+        label: "General under Honorable Conditions",
+        value: "General under Honorable Conditions"
+      },
+      { label: "Other than Honorable", value: "Other than Honorable" },
+      { label: "Bad Conduct", value: "Bad Conduct" },
+      { label: "Dishonorable", value: "Dishonorable" }
+    ];
+  }
 
   async handleRegister() {
     try {
@@ -61,8 +64,8 @@ export default class RegistrationForm extends LightningElement {
         dateOfBirth: this.dateOfBirth,
         phone: this.phone,
         militaryBranch: this.militaryBranch,
-        veteranStatus: this.veteranStatus
-        // dischargeType: this.dischargeType
+        veteranStatus: this.veteranStatus,
+        dischargeType: this.dischargeType
       });
       if (result === "Success") {
         this.errorMessage = "User registered successfully!";
