048b85f5a1793fb3c0c956854b72e44d80455a6a
smhardy143 <hardyshamia@gmail.com>
2025-02-25 00:26:49 -0600

    appeal submission details

--- force-app/main/default/lwc/appealSubmission/appealSubmission.js
+++ force-app/main/default/lwc/appealSubmission/appealSubmission.js
@@ -1,3 +1,54 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import submitAppeal from '@salesforce/apex/AppealSubmissionController.submitAppeal';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class AppealSubmission extends LightningElement {}
+export default class AppealForm extends LightningElement {
+    @track reason = '';
+    @track comments = '';
+    @track hasAttachment = false;
+    @track appealId; // Stores the newly created appeal ID
+
+    handleChange(event) {
+        const field = event.target.dataset.field;
+        this[field] = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    }
+
+    handleSubmit() {
+        const appealRecord = {
+            Reason__c: this.reason,
+            Comments__c: this.comments,
+            Has_Attachment__c: this.hasAttachment
+        };
+
+        submitAppeal({ newAppeal: appealRecord })
+            .then((result) => {
+                this.appealId = result.Id; // Store appeal ID for file upload
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Appeal Submitted Successfully',
+                    variant: 'success'
+                }));
+
+                // Reset form
+                this.reason = '';
+                this.comments = '';
+                this.hasAttachment = false;
+            })
+            .catch(error => {
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Error',
+                    message: error.body.message,
+                    variant: 'error'
+                }));
+            });
+    }
+
+    handleFileUpload(event) {
+        const uploadedFiles = event.detail.files;
+        this.dispatchEvent(new ShowToastEvent({
+            title: 'Success',
+            message: uploadedFiles.length + ' file(s) uploaded successfully.',
+            variant: 'success'
+        }));
+    }
+}
