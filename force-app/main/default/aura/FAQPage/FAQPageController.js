a786493517ef2bbe232ae922ef06d8d6960c4bf1
Kenny <kennycao96@gmail.com>
2025-03-06 15:22:04 -0500

    FAQ component looks modern now.

--- force-app/main/default/aura/FAQPage/FAQPageController.js
+++ force-app/main/default/aura/FAQPage/FAQPageController.js
@@ -1,7 +1,16 @@
 ({
-    toggleAnswer : function(component, event, helper) {
-        let clickedQuestion = event.target;
-        let answerElement = clickedQuestion.nextElementSibling;
-        $A.util.toggleClass(answerElement, 'hide');
+  toggleAnswer: function (component, event) {
+    var target = event.currentTarget;
+    var answer = target.querySelector(".faq-answer");
+
+    // Toggle answer visibility by adding/removing 'show' class
+    if (answer.classList.contains("show")) {
+      answer.classList.remove("show");
+    } else {
+      answer.classList.add("show");
     }
-})
+
+    // Toggle open/closed state for dropdown effect
+    target.classList.toggle("open");
+  }
+});
